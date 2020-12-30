56122ac8f4354fbde0baee44204aef8b94200c29
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-12-29 20:36:45 -0500

    second half of Flask short

--- Flask/tasks/app.py
+++ Flask/tasks/app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+todos = []
+
 @app.route("/")
 def tasks():
-    return "1"
+    return render_template("tasks.html", tasks=todos)
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return "2"
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        new_task = request.form.get("task")
+        todos.append(new_task)
+
+        return redirect("/")
